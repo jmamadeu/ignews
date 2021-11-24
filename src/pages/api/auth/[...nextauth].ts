ab485c1bb05f163fca62758a25e0d0b97ab10c31
João Amadeu <jmamadeu2000@gmail.com>
2021-11-24 02:46:08 +0100

    chore: add fauna db

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,7 @@
+import { query as q } from 'faunadb';
 import NextAuth from 'next-auth';
 import Providers from 'next-auth/providers';
+import { fauna } from '../../../services/fauna';
 
 export default NextAuth({
   providers: [
@@ -8,5 +10,36 @@ export default NextAuth({
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
       scope: 'read:user'
     })
-  ]
+  ],
+
+  callbacks: {
+    signIn: async (user, account, profile) => {
+      try {
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index('user_by_email'),
+                  q.Casefold(user.email as string)
+                )
+              )
+            ),
+            q.Create(q.Collection('users'), { data: { email: user.email } }),
+            q.Get(
+              q.Match(
+                q.Index('user_by_email'),
+                q.Casefold(user.email as string)
+              )
+            )
+          )
+        );
+
+        return true;
+      } catch (err) {
+        console.log(err);
+        return false;
+      }
+    }
+  }
 });
