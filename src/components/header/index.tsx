32b37fb6acc0fa1ef7d29da54853962a8fd9dc40
João Amadeu <jmamadeu2000@gmail.com>
2021-12-03 08:46:43 +0100

    chore: add active link

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,14 +1,33 @@
+import Link from 'next/link';
 import { SignInOrOutButton } from '../sign-in-out-button';
 import styles from './styles.module.scss';
+import { useRouter } from 'next/router';
+import { v4 } from 'uuid';
+
+const routes = [
+  { path: '/', name: 'Home' },
+  { path: '/posts', name: 'Posts' }
+];
 
 export function Header() {
+  const activeRoute = useRouter();
+
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
         <img src="/images/logo.svg" alt="IgNews logo" />
         <nav className={styles.contentContainer}>
-          <a className={styles.active}>Home</a>
-          <a>Posts</a>
+          {routes.map((route) => (
+            <Link key={v4()} href={route.path}>
+              <a
+                className={
+                  route.path === activeRoute.asPath ? styles.active : ''
+                }
+              >
+                {route.name}
+              </a>
+            </Link>
+          ))}
         </nav>
         <SignInOrOutButton />
       </div>
